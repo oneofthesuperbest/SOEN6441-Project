d7b12990509a83fd6ae37e796b785328c6796d0d
Akhil Sharma <akhilsharma.cse@gmail.com>
2021-02-08 07:02:20 +0530

    feature-readmap: Code Refactor for Coordinate, Country and, player models. Renamed data members according to the build document.

--- src/main/java/model/CoordinateModel.java
+++ src/main/java/model/CoordinateModel.java
@@ -4,29 +4,32 @@ package model;
  * Represents the coordinates of a country.
  */
 public class CoordinateModel {
-    private int x;
-    private int y;
+    private int d_x;
+    private int d_y;
 
     /**
      * Creates a coordinate with the specified x and y values.
-     *
-     * @param x x-coordinate value
-     * @param y y-coordinate value
+     * @param p_x x-coordinate value.
+     * @param p_y y-coordinate value.
      */
-    public CoordinateModel(int x, int y){
-        this.x = x;
-        this.y = y;
+    public CoordinateModel(int p_x, int p_y){
+        this.d_x = p_x;
+        this.d_y = p_y;
     }
 
     /**
-     * get the x-coordinate value
-     * @return x-coordinate value
+     * Get the x-coordinate value.
+     * @return x-coordinate value.
      */
-    public int getX() {return x;}
+    public int getX() {
+        return d_x;
+    }
 
     /**
-     * get the y-coordinate value
-     * @return y-coordinate value
+     * Get the y-coordinate value.
+     * @return y-coordinate value.
      */
-    public int getY() {return y;}
+    public int getY() {
+        return d_y;
+    }
 }
