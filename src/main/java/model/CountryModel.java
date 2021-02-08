498af0de2a26a7a691180e93bc11d820e9c4da49
Akhil Sharma <akhilsharma.cse@gmail.com>
2021-02-08 07:02:20 +0530

    feature-readmap: Code Refactor for Coordinate, Country and, player models. Renamed data members according to the build document.

--- src/main/java/model/CountryModel.java
+++ src/main/java/model/CountryModel.java
@@ -4,81 +4,74 @@ package model;
  * Represents a country.
  */
 public class CountryModel {
-	private int order;
-	private String name;
-	private int continentOrder;
-	private CoordinateModel coordinate;
-	private PlayerModel owner;
+	private int d_order;
+	private String d_name;
+	private int d_continentOrder;
+	private CoordinateModel d_coordinate;
+	private PlayerModel d_owner;
 
 	/**
-	 * Creates a country with the specified order, name, continent and, coordinate.
-	 *
-	 * @param order       Order of the country in the map.
-	 * @param name        Name of the country.
-	 * @param continentOrder   The order of the continent that this country belongs to.
-	 * @param coordinate  The Coordinate of this country
+	 * Creates a country with the specified order, name, continent order and, coordinate.
+	 * @param p_order			Order of the country according to the map file.
+	 * @param p_name            Name of the country according to the map file.
+	 * @param p_continentOrder  The order of the continent that this country belongs to.
+	 * @param p_coordinate  	The Coordinate of this country.
 	 */
-	public CountryModel(int order, String name, int continentOrder, CoordinateModel coordinate) {
-		this.order = order;
-		this.name = name;
-		this.continentOrder = continentOrder;
-		this.coordinate = coordinate;
+	public CountryModel(int p_order, String p_name, int p_continentOrder, CoordinateModel p_coordinate) {
+		this.d_order = p_order;
+		this.d_name = p_name;
+		this.d_continentOrder = p_continentOrder;
+		this.d_coordinate = p_coordinate;
 	}
 
 	/**
-	 * Get the country's order.
-	 *
-	 * @return An integer representing the country's order.
+	 * Get the order of the country.
+	 * @return Order of the country.
 	 */
 	public int getOrder() {
-		return this.order;
+		return this.d_order;
 	}
 
 	/**
-	 * Get a country's name.
-	 *
-	 * @return A string representing the country's name.
+	 * Get the name of the country.
+	 * @return Name of the country.
 	 */
 	public String getName() {
-		return this.name;
+		return this.d_name;
 	}
 
 	/**
 	 * Get the continent order of the continent to which this country belongs.
-	 *
 	 * @return The continent order to which this country belongs.
 	 */
 	public int getContinentOrder() {
-		return this.continentOrder;
+		return this.d_continentOrder;
 	}
 
 	/**
 	 * Get the coordinates of this country.
-	 *
-	 * @return The Coordinate object of this country
+	 * @return The coordinates of this country
 	 */
 	public CoordinateModel getCoordinate() {
-		return this.coordinate;
+		return this.d_coordinate;
 	}
 
 	/**
 	 * Get the owner of this country.
-	 *
-	 * @return Player object who owns this country.
+	 * @return The player who owns this country.
 	 */
 	public PlayerModel getOwner() {
-		return this.owner;
+		return this.d_owner;
 	}
 
 	/**
 	 * Set the owner of this country.
-	 *
-	 * @param owner Player that owns this country.
+	 * @param p_owner Player who owns this country.
 	 */
 
-	public void setOwner(PlayerModel owner) {
-		if (owner != this.owner) {
-			this.owner = owner;
+	public void setOwner(PlayerModel p_owner) {
+		if (p_owner != this.d_owner) {
+			this.d_owner = p_owner;
 		}
 	}
 
--- src/main/java/model/PlayerModel.java
+++ src/main/java/model/PlayerModel.java
@@ -3,14 +3,14 @@ package model;
  *  Represents a player
  */
 public class PlayerModel {
-    private String name;
+    private String d_name;
 
     /**
      *  Creates a player with the specified name.
-     *  @param name The players name.
+     *  @param p_name The players name.
      */
-    public PlayerModel(String name){
-        this.name = name;
+    public PlayerModel(String p_name){
+        this.d_name = p_name;
     }
 
     /**
@@ -18,6 +18,6 @@ public class PlayerModel {
      * @return A string representing the player's name.
      */
     public String getName(){
-        return this.name;
+        return this.d_name;
     }
 }
