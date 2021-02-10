4511454da4549deff1bff7fe3da79df288271ef6
Akhil Sharma <akhilsharma.cse@gmail.com>
2021-02-10 23:53:53 +0530

    Feature-mapstate: The map data is now stored in a separate MapState object inside the GameEngine.

--- src/main/java/controller/GameEngine.java
+++ src/main/java/controller/GameEngine.java
@@ -5,60 +5,19 @@ import java.util.ArrayList;
 
 import model.ContinentModel;
 import model.CountryModel;
+import model.MapState;
 
 /**
  * This class is the main game engine which carry out commands of the players
  */
 public class GameEngine {
-	ArrayList<ContinentModel> d_listOfContinents = new ArrayList<ContinentModel>();
-	ArrayList<CountryModel> d_listOfCountries = new ArrayList<CountryModel>();
-	int[][] d_borderGraph;
+	private MapState d_mapState = new MapState();
 
 	/**
-	 * Set the list of continents.
-	 * @param p_listOfContinents List of continents.
+	 * Get the current map state.
+	 * @return Current map state.
 	 */
-	public void setListOfContinents(ArrayList<ContinentModel> p_listOfContinents) {
-		this.d_listOfContinents = p_listOfContinents;
-	}
-
-	/**
-	 * Get the list of continents.
-	 * @return list of continents.
-	 */
-	public ArrayList<ContinentModel> getListOfContinents() {
-		return d_listOfContinents;
-	}
-
-	/**
-	 * Set the list of countries.
-	 * @param p_listOfCountries List of countries.
-	 */
-	public void setListOfCountries(ArrayList<CountryModel> p_listOfCountries) {
-		this.d_listOfCountries = p_listOfCountries;
-	}
-
-	/**
-	 * Get the list of countries.
-	 * @return List of countries.
-	 */
-	public ArrayList<CountryModel> getListOfCountries() {
-		return d_listOfCountries;
-	}
-
-	/**
-	 * Set the border graph.
-	 * @param p_borderGraph Border graph.
-	 */
-	public void setBorderGraph(int[][] p_borderGraph) {
-		this.d_borderGraph = p_borderGraph;
-	}
-
-	/**
-	 * Get the border graph.
-	 * @return The border graph.
-	 */
-	public int[][] getBorderGraph() {
-		return d_borderGraph;
+	public MapState getMapState() {
+		return d_mapState;
 	}
 }
---
+++ src/main/java/model/MapState.java
@@ -0,0 +1,69 @@
+package model;
+
+import java.util.ArrayList;
+
+/**
+ * Represents the current map state inside the GameEngine.
+ */
+public class MapState {
+    ArrayList<ContinentModel> d_listOfContinents = new ArrayList<ContinentModel>();
+    ArrayList<CountryModel> d_listOfCountries = new ArrayList<CountryModel>();
+    int[][] d_borderGraph;
+
+    /**
+     * Set the list of continents.
+     * @param p_listOfContinents List of continents.
+     */
+    public void setListOfContinents(ArrayList<ContinentModel> p_listOfContinents) {
+        this.d_listOfContinents = p_listOfContinents;
+    }
+
+    /**
+     * Get the list of continents.
+     * @return list of continents.
+     */
+    public ArrayList<ContinentModel> getListOfContinents() {
+        return d_listOfContinents;
+    }
+
+    /**
+     * Set the list of countries.
+     * @param p_listOfCountries List of countries.
+     */
+    public void setListOfCountries(ArrayList<CountryModel> p_listOfCountries) {
+        this.d_listOfCountries = p_listOfCountries;
+    }
+
+    /**
+     * Get the list of countries.
+     * @return List of countries.
+     */
+    public ArrayList<CountryModel> getListOfCountries() {
+        return d_listOfCountries;
+    }
+
+    /**
+     * Set the border graph.
+     * @param p_borderGraph Border graph.
+     */
+    public void setBorderGraph(int[][] p_borderGraph) {
+        this.d_borderGraph = p_borderGraph;
+    }
+
+    /**
+     * Get the border graph.
+     * @return The border graph.
+     */
+    public int[][] getBorderGraph() {
+        return d_borderGraph;
+    }
+
+    /**
+     * Clear the map data.
+     */
+    public void clear(){
+        d_listOfContinents = new ArrayList<ContinentModel>();
+        d_listOfCountries = new ArrayList<CountryModel>();
+        d_borderGraph = null;
+    }
+}
