7eea6d5d802e7ed80c547968f26014c3ed1996cb
oneofthesuperbest <tony.wongsuwan@gmail.com>
2021-03-13 14:03:36 +0530

    Generated javadoc

--- src/main/java/model/Observer.java
+++ src/main/java/model/Observer.java
@@ -10,7 +10,7 @@ public interface Observer {
 	 * method to be implemented that reacts to the notification generally by
 	 * interrogating the model object and displaying its newly updated state.
 	 * 
-	 * @param o
+	 * @param o: Reference to the subject class which is being observed for changes.
 	 */
 	public void update(Observable o);
 }
